bf203f5585d19696abdbc14fbefd1f4aa646554e
Arthur Lenz <arthur.lenz@sap.com>
2022-01-29 10:50:01 -0300

    Add billing cmd with UserRegisteredEvent consumer

--- billing/cmd/main.go
+++ billing/cmd/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "log"
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill"
+	watermillAMQP "github.com/ThreeDotsLabs/watermill-amqp/pkg/amqp"
+
+	"github.com/turao/go-ddd/api/amqp"
+)
 
 func main() {
-	log.Println("todo")
+	queue := watermillAMQP.NewDurableQueueConfig("amqp://localhost:5672")
+	logger := watermill.NewStdLogger(false, false)
+
+	subscriber, err := watermillAMQP.NewSubscriber(queue, logger)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer subscriber.Close()
+
+	ures, err := amqp.NewUserRegisteredEventSubscriber(subscriber)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	events, err := ures.Subscribe(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	go func() {
+		for event := range events {
+			d, err := json.MarshalIndent(event, "", " ")
+			if err != nil {
+				log.Fatalln(err)
+			}
+			log.Println("received event:", string(d))
+		}
+	}()
+
+	time.Sleep(30 * time.Second)
 }
