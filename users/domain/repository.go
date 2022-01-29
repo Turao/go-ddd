8f7b738976b352ad7f298b7877c4c1e10852ce07
Arthur Lenz <arthur.lenz@sap.com>
2022-01-29 10:41:37 -0300

    IDK what happened here

--- users/domain/repository.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "context"
-
-type Repository interface {
-	FindByID(ctx context.Context, id UserID) (*User, error)
-	Save(ctx context.Context, user User) error
-	FindAll(ctx context.Context) ([]*User, error)
-}
