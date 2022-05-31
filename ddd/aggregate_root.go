6f753b15b36830f5184321db2399ddbba6e47cca
Turao <lenz.arthur@gmail.com>
2022-05-30 23:58:38 -0300

    Rename aggregate root variables

--- ddd/aggregate_root.go
+++ ddd/aggregate_root.go
@@ -14,13 +14,13 @@ type AggregateRoot struct {
 	EventStore events.EventStore
 }
 
-func NewAggregateRoot(a Aggregate, es events.EventStore) (*AggregateRoot, error) {
+func NewAggregateRoot(agg Aggregate, es events.EventStore) (*AggregateRoot, error) {
 	// limit how long to wait for the re-creation of the aggregate root
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	root := &AggregateRoot{
-		Aggregate:  a,
+		Aggregate:  agg,
 		version:    0,
 		EventStore: es,
 	}
@@ -34,7 +34,7 @@ func NewAggregateRoot(a Aggregate, es events.EventStore) (*AggregateRoot, error)
 
 	log.Println("replaying events")
 	for _, evt := range evts {
-		err := a.HandleEvent(ctx, evt.(events.DomainEvent)) // todo: can we cast these events?
+		err := agg.HandleEvent(ctx, evt.(events.DomainEvent)) // todo: can we cast these events?
 		if err != nil {
 			return nil, err
 		}
@@ -43,30 +43,30 @@ func NewAggregateRoot(a Aggregate, es events.EventStore) (*AggregateRoot, error)
 	return root, nil
 }
 
-func (a *AggregateRoot) HandleEvent(ctx context.Context, evt events.DomainEvent) error {
+func (root *AggregateRoot) HandleEvent(ctx context.Context, evt events.DomainEvent) error {
 	log.Printf("handling event - %s", evt.Name())
 	defer log.Printf("event handled - %s", evt.Name())
-	err := a.Aggregate.HandleEvent(ctx, evt)
+	err := root.Aggregate.HandleEvent(ctx, evt)
 	if err != nil {
 		return err
 	}
-	a.version += 1
+	root.version += 1
 	return nil
 }
 
-func (a *AggregateRoot) HandleCommand(ctx context.Context, cmd interface{}) error {
+func (root *AggregateRoot) HandleCommand(ctx context.Context, cmd interface{}) error {
 	log.Println("handling command")
 	defer log.Println("command handled")
-	evts, err := a.Aggregate.HandleCommand(ctx, cmd)
+	evts, err := root.Aggregate.HandleCommand(ctx, cmd)
 	if err != nil {
 		return err
 	}
 
 	// aggregate root version should increment for each command successfully handled
-	a.version += len(evts)
+	root.version += len(evts)
 
 	for _, evt := range evts {
-		err = a.EventStore.Push(ctx, evt, a.version)
+		err = root.EventStore.Push(ctx, evt, root.version)
 		if err != nil {
 			return err
 		}
@@ -74,7 +74,7 @@ func (a *AggregateRoot) HandleCommand(ctx context.Context, cmd interface{}) erro
 	return nil
 }
 
-func (a *AggregateRoot) CommitEvents() error {
+func (root *AggregateRoot) CommitEvents() error {
 	log.Println("commiting events - todo!") // todo
 	return nil
 }
