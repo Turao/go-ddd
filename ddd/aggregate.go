9fb07e1e6d90fa3b241158056a466d9f648f0a0b
Turao <lenz.arthur@gmail.com>
2022-06-10 23:35:36 -0300

    Extend json.Marshaler/Unmarshaler instead of methods directly

--- ddd/aggregate.go
+++ ddd/aggregate.go
@@ -2,12 +2,15 @@ package ddd
 
 import (
 	"context"
+	"encoding/json"
 )
 
 type Aggregate interface {
 	ID() string
+
 	HandleEvent(ctx context.Context, evt DomainEvent) error
 	HandleCommand(ctx context.Context, cmd interface{}) ([]DomainEvent, error)
-	MarshalJSON() ([]byte, error)    // aggregate must be serializable due to snapshots
-	UnmarshalJSON(data []byte) error // aggregate must be deserializable due to snapshots
+
+	json.Marshaler
+	json.Unmarshaler
 }
